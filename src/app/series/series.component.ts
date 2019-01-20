07558ecd96c143894e328354bdc447ba4b50819c
mossi <mossi.hashemi@gmail.com>
2019-01-20 18:23:57 +0100

    Displaying Series list

--- src/app/series/series.component.ts
+++ src/app/series/series.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Serie } from '../serie';
+import { SERIES } from '../mock-series';
 
 @Component({
   selector: 'app-series',
@@ -8,18 +9,20 @@ import { Serie } from '../serie';
 })
 export class SeriesComponent implements OnInit {
 
-  serie : Serie = {
-   
-      idSERIE: 1,
-      titre: "How I Met Your Mother",
-      description: "Ted has fallen in love. It all started when his best friend, Marshall, drops the bombshell ",
-      annee: "2006",
-      image: "http://www.gstatic.com/tv/thumb/tvbanners/9916255/p9916255_b_v8_aa.jpg",
-  };
+
  
+  series = SERIES;
+  selectedSerie: Serie;
+
+
+
   constructor() { }
 
   ngOnInit() {
   }
 
+
+  onSelect(serie: Serie): void {
+    this.selectedSerie = serie;
+  }
 }
